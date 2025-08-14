afe46da371dae950208536bbc6b890bf3543f816
caiomeirelles22 <dev.meirelles22@gmail.com>
2025-08-14 17:08:23 -0300

    ~po~po

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ItemModule } from './item/item.module';
 import { Item } from './item/item.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), // Load environment variables
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [Item],
-        synchronize: true,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', // endereço do banco
+      port: 5432, // porta
+      username: 'postgres', // usuário
+      password: 'docker', // senha
+      database: 'lista', // nome do banco (precisa criar antes no Postgres)
+      entities: [Item],
+      synchronize: true, // cuidado em produção
     }),
     ItemModule,
   ],
